962d1ac0d1beaa8c7ee16b89fe8f4c12a436bd5d
ssjl06.lee <ssjl06.lee@samsung.com>
2024-05-20 16:51:43 +0900

    implementation of getSum and add some tests

--- team_calc/calc.cpp
+++ team_calc/calc.cpp
@@ -1,7 +1,7 @@
 
 class Calc {
 	int getSum(int a, int b) {
-		return 0;
+		return a + b;
 	}
 	int getGop(int a, int b) {
 		return 0;
--- team_calc/test.cpp
+++ team_calc/test.cpp
@@ -1,8 +1,12 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
+#include "calc.cpp"
 
 TEST(calc, calcSum) {
-	EXPECT_EQ(1, 1);
+	Calc calc;
+	EXPECT_EQ(3, calc.getSum(2, 1));
+	EXPECT_EQ(3, calc.getSum(1, 2));
+	EXPECT_EQ(5, calc.getSum(2, 3));
 }
 
 TEST(calc, calcGop) {

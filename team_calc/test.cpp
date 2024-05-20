165c0cf3b97a53c6541430299bf8b65dded66fa0
iamyunsu <iamyunsu@naver.com>
2024-05-20 16:41:34 +0900

    first commit

---
+++ team_calc/test.cpp
@@ -0,0 +1,26 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+TEST(calc, calcSum) {
+	EXPECT_EQ(1, 1);
+}
+
+TEST(calc, calcGop) {
+	EXPECT_EQ(1, 1);
+}
+
+TEST(calc, calcZegop) {
+	EXPECT_EQ(1, 1);
+}
+
+TEST(calc, calcMinus) {
+	EXPECT_EQ(1, 1);
+}
+
+TEST(calc, calcDivide) {
+	EXPECT_EQ(1, 1);
+}
+
+TEST(calc, calcSumSum) {
+	EXPECT_EQ(1, 1);
+}
